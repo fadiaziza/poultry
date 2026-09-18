a3a1a621a4dcf7e9edc8828c37f0abfa19e6b336
fadiaziza <fadi.mahmood@aziza.ps>
2026-09-18 18:09:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -335,6 +335,5 @@ with gr.Blocks(title="منصة الدعم الهندسي - التعرف البص
     )
 
     gr.HTML(footer_markdown)
-
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=PORT)
+    demo.queue().launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 8080)), inbrowser=False)
