77053c8536e6faf20cf2b20cf0771ddafa73cdac
fadiaziza <fadi.mahmood@aziza.ps>
2026-09-19 23:42:21 +0300

    Restrict allowed_paths to only '/tmp'

--- app.py
+++ app.py
@@ -638,5 +638,5 @@ if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
         server_port=PORT,
-        allowed_paths=[str(BASE_DIR), ".", "/tmp"],
+        allowed_paths=["/tmp"]
     )
